b95562ab8dd310532d19bc8f1255c1952f41a5f4
Brandon Barber <roecrew@gmail.com>
2015-03-19 23:02:43 -0500

    initial

--- app.js
+++ app.js
@@ -1,66 +1,56 @@
 #!/usr/bin/env node
 
 var argv = require('minimist')(process.argv.slice(2), {});
-var mgSrch = require('magsearch');
 var clivas = require('clivas');
+var aneum = require('./');
 var keypress = require('keypress');
 
+var link = [];
 
 keypress(process.stdin);
 process.stdin.setEncoding('utf8');
 
-if(!argv.p) {
-	argv.p=1;
-}
+var searchStr = "";
+var flag = "";
+var txt = "";
 
-if(!argv.k) {
-	argv.k="all";
-}
+var stdin = process.openStdin(); 
+process.stdin.setRawMode(true);
+stdin.on('keypress', function (chunk, key) {
+	if (key == undefined) {searchStr += chunk; process.stdout.clearLine(); process.stdout.cursorTo(0); process.stdout.write(searchStr);}
 
-if(argv._[0] && (argv.s || argv.F)) {
-	if(argv.F) {
-		mgSrch.feelingLucky(argv._[0], function(resultArr) {
-			clivas.line("\n");
-			clivas.line("{bold:"+resultArr[0]+"}");
-			clivas.line("{cyan:"+resultArr[1]+"}");
-			clivas.line("\n");
-		});
-	}
+	else if (key && key.ctrl && key.name == 'c') process.exit();
 
-	else if(argv.s === "btd") {
-		mgSrch.btdigg(argv._[0], argv.p-1, function(resultArr) {
+	else if(key.name == "backspace") {searchStr = searchStr.slice(0, searchStr.length-1);}
+
+	else if(key.name == "return") {
+		console.log("\n");
+		if (searchStr[0] === 'r') {
 			clivas.line("\n");
-			for(var i=resultArr[0].length-1; i>=0; i--) {
-				clivas.line("{bold:"+resultArr[0][i]+"}");
-				clivas.line("{white:"+resultArr[2][i]+"}");
-				clivas.line("{cyan:"+resultArr[1][i]+"}");
+			aneum.select(link[Number(searchStr[2])-1], function(resultArr) {
+				clivas.line("{bold:"+resultArr+"}");
+			});
+		}
+		else if (searchStr[0] === 's') {
+			aneum.search(searchStr.slice(1,-1).trim(), function(resultArr) {
+				searchStr = "";
 				clivas.line("\n");
-			}
-		});
+				for(var i=resultArr[0].length-1; i>=0; i--) {
+					clivas.line("{bold:"+(i+1)+' '+resultArr[0][i]+"}");
+					clivas.line("{cyan:  "+resultArr[1][i]+"}");
+					clivas.line("{green:  "+resultArr[2][i]+"}");
+					clivas.line("{orange:  "+resultArr[3][i]+"}");
+					clivas.line("\n");
+				}
+				link = resultArr[3];
+			});
+		}
 	}
+	else { searchStr += chunk;}
+	process.stdout.clearLine();
+	process.stdout.cursorTo(0);
+	process.stdout.write(searchStr);
+});
 
-	else if(argv.s === "tpb") {
-		mgSrch.pbay(argv._[0], argv.p-1, argv.k, function(resultArr) {
-			clivas.line("\n");
-			for(var i=resultArr[0].length-1; i>=0; i--) {
-				clivas.line("{bold:"+resultArr[0][i]+"}");
-				clivas.line("{cyan:"+resultArr[1][i]+"}");
-				clivas.line("{green:"+resultArr[2][i]+"}");
-				clivas.line("{red:"+resultArr[3][i]+"}");
-				clivas.line("\n");
-			}
-		});
-	}
-	else if(argv.s === "opb") {
-		mgSrch.oldpbay(argv._[0], argv.p, argv.k, function(resultArr) {
-			clivas.line("\n");
-			for(var i=resultArr[0].length-1; i>=1; i--) {
-				clivas.line("{bold:"+resultArr[0][i]+"}");
-				clivas.line("{cyan:"+resultArr[1][i]+"}");
-				clivas.line("{green:"+resultArr[2][i]+"}");
-				clivas.line("{red:"+resultArr[3][i]+"}");
-				clivas.line("\n");
-			}
-		});
-	}
-}
+
+
--- index.js
+++ index.js
@@ -1,3 +1,75 @@
-/**
- * New node file
- */
+var request = require('request');
+var cheerio = require('cheerio');
+
+exports.search = function(s, callback) {
+	var ss="";
+	var retArr = [];
+	for(var tmp=0; tmp<s.length; tmp++) {
+		if(s.charAt(tmp) === ' ') {
+			ss+='%20';
+		}
+		else {
+			ss+=s.charAt(tmp);
+		}
+	}
+	var url = 'https://www.gutenberg.org/ebooks/search/?query='+ss;
+	request(url, function(error, response, html){
+		var title = [];
+		var author = [];
+		var download = [];
+		var link = [];
+		if(!error){
+			var i=0;
+			var $ = cheerio.load(html);
+//			if($('#recaptcha_response_field') !== undefined) {
+//				console.log("captcha prompted...");
+//				//console.log($('#recaptcha_image').filter("[width]").attr('width'));
+//			}
+			$('.booklink').each(function(a, b){
+				title.push($('.title', b).text());
+				if($('.subtitle', b).text() === "") {
+					author.push("N.A");
+				}
+				else {
+					author.push($('.subtitle', b).text());
+				}
+				download.push($('.extra', b).text());
+				link.push($('a', b).filter("[href]").attr('href'));
+			});
+		}
+		retArr[0]=title;
+		retArr[1]=author;
+		retArr[2]=download;
+		retArr[3]=link;
+		return callback(retArr);
+	});
+};
+
+exports.select = function(s, callback) {
+	var ss="";
+	var retArr;
+	for(var tmp=0; tmp<s.length; tmp++) {
+		if(s.charAt(tmp) === ' ') {
+			ss+='%20';
+		}
+		else {
+			ss+=s.charAt(tmp);
+		}
+	}
+	
+	var url = 'https://www.gutenberg.org'+ss;
+	request(url, function(error, response, html){
+		if(!error){
+			var i=0;
+			var $ = cheerio.load(html);
+			$('.link').filter(function(a, b){
+				//console.log($(this).text());
+				if($(this).attr('type') === "text/plain") {
+					request("https:"+$(this).attr('href'), function(error, response, html){
+						return callback(response.body);
+					});
+				}
+			});
+		}
+	});
+};
